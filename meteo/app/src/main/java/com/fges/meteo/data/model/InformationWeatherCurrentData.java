cee7afff95f212eda86d08a2c261ea65d214d35b
Steven Dieu <groover.Dieu@gmail.com>
2017-04-30 16:56:33 +0200

    Add dabatase

--- meteo/app/src/main/java/com/fges/meteo/data/model/InformationWeatherCurrentData.java
+++ meteo/app/src/main/java/com/fges/meteo/data/model/InformationWeatherCurrentData.java
@@ -1,16 +1,23 @@
 package com.fges.meteo.data.model;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by steven on 18/04/2017.
  */
 
+@DatabaseTable(tableName = "information_weather_current")
 public class InformationWeatherCurrentData implements IIformationWeatherData {
+    @DatabaseField(generatedId = true)
+    private int time;
+
+    @DatabaseField(canBeNull = false)
     private String summary;
     private String icon;
     private double temperature;
     private double humidity;
     private double windSpeed;
-    private int time;
     private double precipIntensity;
     private double precipProbability;
     private double cloudCover;
